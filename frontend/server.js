ef9b9783e8b2cfcffebdfcb2a3ba0ed76e4c1a02
RiyaJadhav1803 <166241201+RiyaJadhav1803@users.noreply.github.com>
2024-07-17 11:35:42 +0530

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const mongoose=require("mongoose");
 const app=express();
-const PORT=2020;
+const PORT=process.env.PORT|| 2020;
 require('dotenv').config();
 const nodemailer=require("nodemailer");
 const ejsLay=require("express-ejs-layouts");
